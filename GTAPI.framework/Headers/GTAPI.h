ea7fd831b249decec5d0f7705d2f657ea152a84b
zh神夕 <kkbbggtt@163.com>
2019-03-11 12:24:16 +0800

    zzz
    Signed-off-by: zh神夕 <kkbbggtt@163.com>

---
+++ GTAPI.framework/Headers/GTAPI.h
@@ -0,0 +1,36 @@
+//
+//  GTAPI.h
+//  GTAPI
+//
+//  Created by 张兵 on 2019/3/11.
+//  Copyright © 2019 张兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GTAPI.
+FOUNDATION_EXPORT double GTAPIVersionNumber;
+
+//! Project version string for GTAPI.
+FOUNDATION_EXPORT const unsigned char GTAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GTAPI/PublicHeader.h>
+
+#import <GTAPI/APIWindowContainer.h>
+#import <GTAPI/APIWidgetContainer.h>
+#import <GTAPI/APIManager.h>
+#import <GTAPI/APIEvent.h>
+#import <GTAPI/APIUpdateManager.h>
+#import <GTAPI/APIWebView.h>
+#import <GTAPI/APIScriptMessage.h>
+#import <GTAPI/APIModuleMethod.h>
+
+#import <GTAPI/UZAppDelegate.h>
+#import <GTAPI/UZWidget.h>
+#import <GTAPI/UZAppUtils.h>
+#import <GTAPI/UZModule.h>
+#import <GTAPI/WebView.h>
+#import <GTAPI/NSDictionaryUtils.h>
+#import <GTAPI/UZModule.h>
+
+
